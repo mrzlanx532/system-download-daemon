c931d57ee7f582f165bd9c21c80156e6c312a2b1
Denis Danilov <dd@sst.cat>
2020-01-13 14:18:24 +0300

    Set handlers to buttons (btn_images, btn_pdf, btn_other_file, btn_save_settings)
    1. Added action that sets browse_path to inputs
    2. Test work with ProgressBar

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import time
 import tkfilebrowser
 from PyQt5 import uic
-from PyQt5.QtWidgets import QPushButton, QLineEdit, QApplication, QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QPushButton, QLineEdit, QApplication, QMainWindow, QFileDialog, QProgressBar
 
 
 class Window(QMainWindow):
@@ -13,15 +14,54 @@ class Window(QMainWindow):
         super(Window, self).__init__()
         uic.loadUi('ui/interface.ui', self)
 
-        btn_browse_pictures = self.button_images_browse
-        btn_browse_pictures.clicked.connect(self.button_images_browse_)
+        button_images_browse = self.button_images_browse
+        button_images_browse.clicked.connect(self.button_images_browse_handler)
+
+        button_pdf_browse = self.button_pdf_browse
+        button_pdf_browse.clicked.connect(self.button_pdf_browse_handler)
+
+        button_other_files_browse = self.button_other_files_browse
+        button_other_files_browse.clicked.connect(self.button_other_files_browse_handler)
+
+        button_save_settings = self.button_save_settings
+        if button_save_settings.isEnabled():
+            button_save_settings.clicked.connect(self.button_save_settings_handler)
 
         self.show()
 
-    def button_images_browse_(self):
-        input_pictures = self.input_images
+    def button_images_browse_handler(self):
         dirname = QFileDialog.getExistingDirectory()
+        input_pictures = self.input_images
         input_pictures.setText(dirname)
+
+    def button_pdf_browse_handler(self):
+        dirname = QFileDialog.getExistingDirectory()
+        input_pdf = self.input_pdf
+        input_pdf.setText(dirname)
+
+    def button_other_files_browse_handler(self):
+        dirname = QFileDialog.getExistingDirectory()
+        input_other_files = self.input_other_files
+        input_other_files.setText(dirname)
+
+    def button_save_settings_handler(self):
+        progressBar = self.progressBar
+
+        button_save_settings = self.button_save_settings
+
+        #print(button_save_settings.isEnabled())
+
+        button_save_settings.setEnabled(False)
+
+        if progressBar.value() == 0 or progressBar.value() == 100:
+            i = 0
+            while i != 101:
+                progressBar.setValue(i)
+                time.sleep(0.01)
+                i = i + 1
+
+        button_save_settings.setEnabled(True)
+
 
 
 if __name__ == '__main__':
